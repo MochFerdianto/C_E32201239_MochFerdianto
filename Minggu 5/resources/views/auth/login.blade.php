a73764f4221a8ab421294a0819143d3aa03d94a5
MochFerdianto <didibfc1@gmail.com>
2021-05-31 10:09:32 +0700

    Tugas Minggu 5

---
+++ Minggu 5/resources/views/auth/login.blade.php
@@ -0,0 +1,38 @@
+@extends('backend/layouts.login')
+@section('content')
+<form class="login-form" action="index.html">
+    @csrf
+      <div class="login-wrap">
+        <p class="login-img"><i class="icon_lock_alt"></i></p>
+        <div class="input-group">
+          <span class="input-group-addon"><i class="icon_profile"></i></span>
+          <input placeholder="Email" id="email" type="Email"
+            class="form-control @error('email') is-ivalid @enderror" name="email" required
+            autocomplete="email" placeholder="Email">
+          @error('password')
+          <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+          </span>
+          @enderror
+        </div>
+        <div class="input-group">
+          <span class="input-group-addon"><i class="icon_key_alt"></i></span>
+          <input placeholder="Password" id="password" type="Password"
+            class="form-control @error('password') is-ivalid @enderror" name="password" required
+            autocomplete="current-password" placeholder="Password">
+          @error('password')
+          <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+          </span>
+          @enderror
+        </div>
+        <label class="checkbox">
+              <input type="checkbox" value="remember-me" name="remember" id="remember"
+                {{ old('remember') ? 'checked' : '' }}> Remember me
+              <span class="pull-right"><a href="{{ route('password.request') }}"> Forgot Password?</a></span>  
+            </label>
+        <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+        <button class="btn btn-info btn-lg btn-block" type="submit">Signup</button>
+      </div>
+    </form>
+@endsection
